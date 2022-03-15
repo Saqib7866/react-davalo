5fac904bbab939344515147e7413ec22debe4731
Saqib Ali <saqibali@aquila360.com>
2022-03-15 17:59:22 +0500

    set up react project static

--- web-app/src/components/firebase/firebaseService.js
+++
@@ -1,5 +0,0 @@
-import { create } from "apisauce";
-export const fireBaseApi = create({
-  baseURL:
-    "https://securetoken.googleapis.com/v1/token?key=AIzaSyATAO-Cg4LXrdtUufW7LDpJJyEbNAkdgwc",
-});
--- web-app/src/components/models/cartProducts.js
+++
@@ -1,9 +0,0 @@
-const cartProducts = [
-  { productId: 2, quantity: 0 },
-  { productId: 3, quantity: 0 },
-];
-
-
-export function getCartProducts() {
-    return cartProducts;
-  }
